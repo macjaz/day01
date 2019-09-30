a351ce83f32d94bcfcf4410b9443267cff9d2b88
wangcj03 <itw_wangcj03@tkmail.com>
2019-09-30 14:08:43 +0800

    first commit

---
+++ good/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Triton-ale-Demo
+//
+//  Created by Kinno_Wang on 2019/7/22.
+//  Copyright © 2019 Kinno_Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
